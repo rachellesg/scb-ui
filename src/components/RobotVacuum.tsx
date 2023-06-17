69853e85dcbc8e6f1ba9e53bd8d37436cd40d606
Rachelle <rachelle.yeo@outlook.com>
2023-06-17 13:48:51 +0800

    init with grid and position and place action

---
+++ src/components/RobotVacuum.tsx
@@ -0,0 +1,72 @@
+import React, { useState } from 'react'
+
+interface Position {
+  x: number | null
+  y: number | null
+  f: 'NORTH' | 'EAST' | 'SOUTH' | 'WEST'
+  placed: boolean | false
+}
+
+const RobotVacuum: React.FC = () => {
+  const [position, setPosition] = useState<Position | null>(null)
+
+  const GRID_SIZE = 5
+
+  const handlePlace = () => {
+    const placedPosition: Position = {
+      x: 0,
+      y: 0,
+      f: 'NORTH',
+      placed: true
+    }
+
+    setPosition(placedPosition)
+  }
+
+  return (
+    <div>
+      <button onClick={handlePlace}>PLACE</button>
+
+      {!position ? (
+        ''
+      ) : (
+        <div className="actions">
+          <input
+            type="text"
+            pattern="\d*"
+            maxLength={1}
+            value={position?.x || 0}
+            onChange={(e) => console.log(e.target.value)}
+          />
+        </div>
+      )}
+
+      <div className="grid">
+        {(() => {
+          const cells = []
+          for (let rowIndex = 0; rowIndex < GRID_SIZE; rowIndex++) {
+            const row = []
+            for (let colIndex = 0; colIndex < GRID_SIZE; colIndex++) {
+              row.push(
+                <div
+                  key={colIndex}
+                  className={`cell ${
+                    position?.x === colIndex && position?.y === rowIndex ? 'robot' : ''
+                  }`}
+                ></div>
+              )
+            }
+            cells.push(
+              <div key={rowIndex} className="row">
+                {row}
+              </div>
+            )
+          }
+          return cells
+        })()}
+      </div>
+    </div>
+  )
+}
+
+export default RobotVacuum
